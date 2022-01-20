aa9a69239814d42729eeeaff8c1216d5100e9a6a
Akiko Green <agreen@alphaworks.tech>
2022-01-20 11:42:55 -0800

    routes for login and out done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 
-import logo from './logo.svg';
+//components
 import Home from './components/Home';
+import Login from './components/Logs /Login';
+import Logout from './components/Logs /Logout';
+//routes
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
+//utilities
 import {
   getRecipe,
   getRecipeBySearch,
@@ -13,10 +17,14 @@ import Search from "./components/Search/Search";
 function App() {
   return (
     <div>
+
         <Search getRecipeBySearch={getRecipeBySearch} />
         {/* <button onClick={() => getIngredientsByRecipeId()}>Get Data</button> */}
       <Routes>
         <Route path='/' element={<Home />} />
+
+        <Route exact path="/login" element={<Login />}/>
+        <Route exact path="/logout" element={<Logout />}/>
       </Routes>
     </div>
   );
