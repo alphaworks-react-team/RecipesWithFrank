d844f2bbf0dcbdf7091be1228da66a2e346db395
Akiko Green <agreen@alphaworks.tech>
2022-01-20 15:29:08 -0800

    feat(login):currently building login component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,81 @@
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 //components
 import Home from "./components/Home";
 import Login from "./components/Logs /Login";
-import Logout from "./components/Logs /Logout";
 //routes
 import { Routes, Route } from "react-router-dom";
+
 import "./App.css";
 //utilities
 
-import {
-  getRecipe,
-  getRecipeBySearch,
-  getIngredientsByRecipeId,
-} from "./utils";
-import Search from "./components/Search/Search";
+// import {
+//   getRecipe,
+//   getRecipeBySearch,
+//   getIngredientsByRecipeId,
+// } from "./utils";
+// import Search from "./components/Search/Search";
 
 function App() {
+  const navigate = useNavigate();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState("");
+  const [auth, setAuth] = useState(false)
+  const [authError, setAuthError] = useState(false)
+
+
+  const getUser = (e) => {
+    console.log(e.target.value)
+    setUsername(e.target.value);
+  };
+
+  const getPassword = (e) => {
+    console.log(e.target.value)
+    setPassword(e.target.value);
+  };
+
+  const authUser = (e) =>{
+    e.preventDefault()
+    // navigate("/");
+    if(!user || !password){
+      setAuthError(true)
+     
+    }
+    const savedUser = JSON.parse(localStorage.getItem("user"))
+    const savedPassword = JSON.parse(localStorage.getItem("password"))
+    if(savedUser !== user && savedPassword !== password){
+      setAuthError(true)
+      alert("please sign up first")
+    } else if(savedUser === user && savedPassword === password){
+      setAuthError(false)
+      setAuth(true)
+      document.querySelector(".username").value = ""
+      document.querySelector(".password").value = ""
+
+    }
+  }
+
   return (
     <div>
-      <Search getRecipeBySearch={getRecipeBySearch} />
-      {/* <button onClick={() => getIngredientsByRecipeId()}>Get Data</button> */}
       <Routes>
         <Route path="/" element={<Home />} />
 
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/logout" element={<Logout />} />
+        <Route
+          exact
+          path="/login"
+          element={
+            <Login
+              getUser={getUser}
+              getPassword={getPassword}
+              user={user}
+              password={password}
+              authUser={authUser}
+              username={username}
+            />
+          }
+        />
       </Routes>
     </div>
   );
