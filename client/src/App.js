2b139928197233251d884b578de838de7df196b9
seenanadimi <78619195+seenanadimi@users.noreply.github.com>
2022-01-20 17:34:59 -0800

    changed link, fixed import error on app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
-
 import { useState } from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 //components
-
+import Navbar from "./components/Navbar/Navbar.js";
 import Home from "./components/Home";
-import Login from "./components/Logs /Login";
+import Login from "./components/Login/Login";
 //routes
 import { Routes, Route } from "react-router-dom";
 
@@ -24,46 +23,43 @@ function App() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [user, setUser] = useState("");
-  const [auth, setAuth] = useState(false)
-  const [authError, setAuthError] = useState(false)
-
+  const [auth, setAuth] = useState(false);
+  const [authError, setAuthError] = useState(false);
 
   const getUser = (e) => {
-    console.log(e.target.value)
+    console.log(e.target.value);
     setUsername(e.target.value);
   };
 
   const getPassword = (e) => {
-    console.log(e.target.value)
+    console.log(e.target.value);
     setPassword(e.target.value);
   };
 
-  const authUser = (e) =>{
-    e.preventDefault()
+  const authUser = (e) => {
+    e.preventDefault();
     // navigate("/");
-    if(!user || !password){
-      setAuthError(true)
-     
+    if (!user || !password) {
+      setAuthError(true);
     }
-    const savedUser = JSON.parse(localStorage.getItem("user"))
-    const savedPassword = JSON.parse(localStorage.getItem("password"))
-    if(savedUser !== user && savedPassword !== password){
-      setAuthError(true)
-      alert("please sign up first")
-    } else if(savedUser === user && savedPassword === password){
-      setAuthError(false)
-      setAuth(true)
-      document.querySelector(".username").value = ""
-      document.querySelector(".password").value = ""
-
+    const savedUser = JSON.parse(localStorage.getItem("user"));
+    const savedPassword = JSON.parse(localStorage.getItem("password"));
+    if (savedUser !== user && savedPassword !== password) {
+      setAuthError(true);
+      alert("please sign up first");
+    } else if (savedUser === user && savedPassword === password) {
+      setAuthError(false);
+      setAuth(true);
+      document.querySelector(".username").value = "";
+      document.querySelector(".password").value = "";
     }
-  }
+  };
 
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-
         <Route
           exact
           path="/login"
