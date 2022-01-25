d83cf8224e94d8ebe773d789c4502cdd5b45bc14
Ariel Agress <ari@ariels-mac-mini.attlocal.net>
2022-01-24 16:34:10 -0800

    Add(Footer): Added two Footer's. FooterPremium.js/Footer.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 //components
 
-import Home from "./components/Home/Home";
-import Navbar from "./components/Navbar/Navbar.js";
-import Login from "./components/Login/Login";
-//routes
-import { Routes, Route } from "react-router-dom";
+import Home from './components/Home/Home';
+import Navbar from './components/Navbar/Navbar.js';
+import Login from './components/Login/Login';
+import FooterPremium from './components/Footer/FooterPremium';
+import Footer from './components/Footer/Footer';
 
-import "./App.css";
+//routes
+import { Routes, Route } from 'react-router-dom';
+
+import './App.css';
 //utilities
 
 // import {
@@ -21,38 +24,38 @@ import "./App.css";
 function App() {
   const navigate = useNavigate();
 
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [user, setUser] = useState("");
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState('');
   const [auth, setAuth] = useState(false);
   const [authError, setAuthError] = useState(false);
 
-  const getUser = (e) => {
+  const getUser = e => {
     console.log(e.target.value);
     setUsername(e.target.value);
   };
 
-  const getPassword = (e) => {
+  const getPassword = e => {
     console.log(e.target.value);
     setPassword(e.target.value);
   };
 
-  const authUser = (e) => {
+  const authUser = e => {
     e.preventDefault();
     // navigate("/");
     if (!user || !password) {
       setAuthError(true);
     }
-    const savedUser = JSON.parse(localStorage.getItem("user"));
-    const savedPassword = JSON.parse(localStorage.getItem("password"));
+    const savedUser = JSON.parse(localStorage.getItem('user'));
+    const savedPassword = JSON.parse(localStorage.getItem('password'));
     if (savedUser !== user && savedPassword !== password) {
       setAuthError(true);
-      alert("please sign up first");
+      alert('please sign up first');
     } else if (savedUser === user && savedPassword === password) {
       setAuthError(false);
       setAuth(true);
-      document.querySelector(".username").value = "";
-      document.querySelector(".password").value = "";
+      document.querySelector('.username').value = '';
+      document.querySelector('.password').value = '';
     }
   };
 
@@ -60,10 +63,10 @@ function App() {
     <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path='/' element={<Home />} />
         <Route
           exact
-          path="/login"
+          path='/login'
           element={
             <Login
               getUser={getUser}
@@ -76,6 +79,8 @@ function App() {
           }
         />
       </Routes>
+      <FooterPremium />
+      <Footer />
     </div>
   );
 }
